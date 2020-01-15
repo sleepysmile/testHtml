8aafb88788180c556b9efbbce37d7ae75defd6bd
roman <ostrowskijroman@gmail.com>
2020-01-15 20:44:14 +0300

    add ditail

--- script/script.js
+++ script/script.js
@@ -1,3 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    lazyLoadImage()
+})
+
 document.querySelector('.menu-btn').addEventListener('click', function () {
     this.parentNode.querySelector('.nav-content').classList.toggle('active');
 })
@@ -5,3 +9,20 @@ document.querySelector('.menu-btn').addEventListener('click', function () {
 document.querySelector('.btn-close').addEventListener('click', function () {
     this.parentNode.classList.toggle('active')
 })
+
+function lazyLoadImage() {
+    const docImage = document.querySelectorAll('img[data-src]');
+    const imageObserver = new IntersectionObserver((entries, imgObserver) => {
+        entries.forEach((entry) => {
+            if(entry.isIntersecting) {
+                let image = entry.target;
+                image.src = image.dataset.src;
+                imageObserver.unobserve(image)
+            }   
+        })
+    });
+
+    docImage.forEach(element => {
+        imageObserver.observe(element);
+    })
+}
